0a820cd080dad741f49f19215f4ce53567ab66ad
Irene Waweru <85219856+irenenjoki@users.noreply.github.com>
2023-11-04 00:18:00 +0300

    Add files via upload

--- book.php
+++ book.php
@@ -12,7 +12,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $names = isset($_POST["names"]) ? $_POST["names"] : "";
     $email = isset($_POST["email"]) ? $_POST["email"] : "";
-    $package = isset($_POST["package"]) ? $_POST["package"] : "";
+    $numberofperson = isset($_POST["numberofperson"]) ? $_POST["numberofperson"] : "";
     $phone = isset($_POST["phone"]) ? $_POST["phone"] : "";
     $date = isset($_POST["date"]) ? $_POST["date"] : "";
     $time = isset($_POST["time"]) ? $_POST["time"] : "";
@@ -22,10 +22,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
 
     // Use prepared statement
-    $stmt = $conn->prepare("INSERT INTO packages (names, email, package,phone, date, time, payment, message) VALUES ( ?, ?, ?, ?, ?, ?, ?,?)");
+    $stmt = $conn->prepare("INSERT INTO reservations (names, email, numberofperson,phone, date, time, payment, message) VALUES ( ?, ?, ?, ?, ?, ?, ?,?)");
     
     
-    $stmt->bind_param("ssssssss", $names, $email, $package, $phone, $date, $time, $payment, $message);
+    $stmt->bind_param("ssisssss", $names, $email,$numberofperson, $phone, $date, $time, $payment, $message);
 
     if ($stmt->execute()) {
         echo "Data has been successfully inserted into the database.";
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 var_dump($names);
 var_dump($phone);
 var_dump($email);
-var_dump($package);
+var_dump($numberofperson);
 var_dump($time);
 var_dump($date);
 var_dump($payment);
